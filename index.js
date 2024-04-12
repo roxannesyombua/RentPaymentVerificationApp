75d07cb50e1e87f936070a67ab82ba3fbf4da5b0
roxannesyombua <roxannesyombua@gmail.com>
2024-04-12 11:50:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,121 @@
+//Event Listeners
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelector('#tenant-profile').addEventListener('submit', handleSubmit)
+});
+
+//Event handlers
+function handleSubmit(e) {
+    e.preventDefault()
+    let tenantObj = {
+        name: e.target.name.value,
+        rent: e.target.rent.value,
+        modeofpayment: e.target.modeofpayment.value,
+        rentpaid: 0
+    }
+    loadOneTenant(tenantObj);
+    addTenant(tenantObj)
+
+}
+
+//DOM Load Function
+function loadOneTenant(tenant) {
+    //Create Tenant
+    let form = document.createElement('li')
+    form.className = "form"
+    form.innerHTML = `
+   <div class ="content">
+    <h4>${tenant.name}</h4>
+    <p> 
+    $<span class="rent-paid">${tenant.rent}</span> Paid
+    </p>
+    <p>Mode of Payment- ${tenant.modeofpayment}</p>
+   </div>
+   <div class="buttons">
+   <button>Rent-Paid</button>
+   <button>Remove-Tenant</button>
+   </div>
+   `
+    form.querySelector("button").addEventListener('click', () => {
+        //console.log("Button clicked");
+        const tenantElement = form.querySelector(".remove-tenant");
+        if (tenantElement) {
+            tenantElement.parentNode.removeChild(tenantElement);
+        }
+        deleteTenant(tenant.id)
+    });
+
+
+    form.querySelector('button').addEventListener('click', () => {
+        console.log("Button clicked");
+        tenant.rent += 1000;
+        const paidRent = parseInt(form.querySelector(".rent-paid").textContent);
+        form.querySelector(".rent-paid").textContent = paidRent + 1000;
+        console.log()
+
+        updateRent(tenant);
+    });
+
+    //  form.querySelector('#Rent-Paid'), addEventListener('click', () => {
+    //     tenant.rent += 1000
+    //      form.querySelector("span").textContent = tenant.rent
+    //      updateRent(tenant)
+    // })
+
+    //Add Tenant Form to DOM
+    document.querySelector("#tenants").appendChild(form)
+}
+
+//Fetch Requests
+//Get Fetch for all Tenants
+function getTenants() {
+    fetch("http://localhost:3000/tenants")
+        .then(response => response.json())
+        .then(tenants => tenants.forEach(tenant => loadOneTenant(tenant)))
+
+}
+
+function addTenant(tenantObj) {
+    fetch("http://localhost:3000/tenants", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tenantObj)
+    })
+        .then(res => res.json())
+        .then(tenant => console.log(tenant))
+}
+
+function deleteTenant(id) {
+    fetch(`http://localhost:3000/tenants/${id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(tenant => console.log(tenant))
+}
+
+function updateRent(id) {
+    fetch(`http://localhost:3000/tenants/${id}`)
+    method: 'PATCH'
+    headers: {
+        'Content-Type'; 'application/json'
+    }
+    body: JSON.stringify(id)
+
+        .then(res => res.json())
+        .then(tenant => console.log(tenant))
+}
+
+//Initial Load
+//Get Data and Load Tenants to the DOM
+
+function initialize() {
+    //tenants.forEach(tenant =>loadOneTenant(tenant))
+    getTenants()
+}
+
+initialize();
